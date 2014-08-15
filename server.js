a1948a8084907c56763bc8613a7989b8a6df8e2e
hwang3419 <hwang.iit@gmail.com>
2014-08-15 16:00:34 -0500

    add test

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
-var PORT = process.env.OPENSHIFT_NODEJS_PORT || 8000;
-var IPADDRESS = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var express  = require('express')
+    , app    = express()
+    , server = require('http').createServer(app)
+    , io     = require('socket.io').listen(server)
+    //, connect = require('connect')
+    //, pg     = require('pg')
+    //, Client = pg.Client;
 
-server = require('http').createServer(app);
-//server.listen(PORT, IPADDRESS);
-http.listen(PORT, function(){
-  console.log('listening on *:80');
-});
+// Setup express middleware
+app.use(express.static('public'));
+//app.use(connect.logger());
+
+// Start the server
+var port = process.env.OPENSHIFT_INTERNAL_PORT || 8000
+    , ip = process.env.OPENSHIFT_INTERNAL_IP || "127.0.0.1";
+server.listen(port, ip);
+
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
